887f412054d1d3a2ad252e9cf8ef69c9c3c57a3f
Goviindsain <gsksain267@gmail.com>
2024-12-30 00:02:37 +0530

    made some configuration on cors

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://post-something-frontend.vercel.app'], // Allow your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Include credentials if necessary
+  }));
 app.use(express.json()); // Parse JSON body
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
